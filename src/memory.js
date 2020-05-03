0d30b1973634682e0ae3b3cbbcd1cd5b3e13ba0b
Tarik <tariksnow37@gmail.com>
2020-05-03 16:47:22 -0700

    build memory module

---
+++ src/memory.js
@@ -0,0 +1,9 @@
+module.export = {
+    memoryUsage: function(callback, options) {
+        callback;
+        const used = process.memoryUsage();
+        for (let key in used) {
+            console.log(`Memory(${key}): ${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`);
+        };
+    },
+}
