a51fd40197f11da351eebda13194028e492b300a
Tarik <tariksnow37@gmail.com>
2020-05-04 08:47:03 -0700

    add system runtime

--- src/runtime.js
+++ src/runtime.js
@@ -2,6 +2,8 @@ const { performance } = require('perf_hooks');
 
 module.exports = {
     executionTime: function (callback) {
+        // This method is based on the machine you are running as well.
+        // Therefore is not the most reliable way to find a system runtime.
         let start = performance.now();
 
         callback;
@@ -9,4 +11,30 @@ module.exports = {
         let end = performance.now();
         console.log(`Runtime: ${end - start}ms`);
     },
+    systemRuntime: function(clockTime) {
+        let system = 0;
+
+        let start = new Date().getTime();
+
+        // Send to the event loop too see how heavy the load is on the CPU
+        setTimeout(() => {
+            let end = new Date().getTime();
+            console.log(end - start);
+            console.log('If there is a large difference between ')
+        }, 0)
+        let end = start;
+
+        while (end - start === 0) {
+            end = new Date().getTime();
+        }
+
+        start = end;
+
+        while (end - start < clockTime) {
+            system++;
+            end = new Date().getTime();
+        }
+        console.log('this is the systems counter', system)
+        return system;
+    }
 }
