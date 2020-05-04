7b248165cf27b1cf368a3e825ebf970e1c7a3668
Tarik <tariksnow37@gmail.com>
2020-05-04 09:15:38 -0700

    comments

--- src/runtime.js
+++ src/runtime.js
@@ -17,6 +17,7 @@ module.exports = {
         let start = new Date().getTime();
 
         // Send to the event loop too see how heavy the load is on the CPU
+        // This will give us a second reference for system runtime
         setTimeout(() => {
             let end = new Date().getTime();
             console.log('start:', start);
@@ -31,6 +32,8 @@ module.exports = {
 
         start = end;
 
+        // Once passed the clocktime input then systems iteration will finish at
+        // the setTimeout which is waiting in the eventloop
         while (end - start < clockTime) {
             system++;
             end = new Date().getTime();
